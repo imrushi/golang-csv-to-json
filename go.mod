e07ac7c7f4de8afd413b75e1df92e262a723c649
imrushi <rushipanchariya19@gmail.com>
2021-06-10 14:18:03 +0530

    Cobara cli package is added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab360.enlight.dev/rushi.panchariya/csv2json
+
+go 1.16
+
+require github.com/spf13/cobra v1.1.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "gitlab360.enlight.dev/rushi.panchariya/csv2json/cmd"
+
+func main() {
+	cmd.Execute()
+}
